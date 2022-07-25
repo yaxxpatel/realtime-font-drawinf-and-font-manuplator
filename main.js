da574d820faa96efffe41719f0c1cffc5ce82494
yaxxpatel <88316870+yaxxpatel@users.noreply.github.com>
2022-07-25 19:32:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size  (550,500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    background ("#068278");
+}
+
+function modelLoaded()
+{
+    console.log("MODEL LOADED")
+}
+
+function gotPoses(results)
+{
+    if(results.length   > 0)
+    {
+        console.log(results);
+    }
+}
+
